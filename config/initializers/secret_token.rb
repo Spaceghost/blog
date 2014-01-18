1469be318fd9038dd514209c4121db9428bf3d35
Johnneylee Jack Rollins <Johnneylee.Rollins@gmail.com>
2014-01-17 22:56:58 -0800

    Generating application using rails 4.0.2 and ruby 2.1.0p0
    The application was generated using:
    `bundle exec rails new blog --skip-active-record --skip-test-unit`
    
    Signed-off-by: Johnneylee Jack Rollins <Johnneylee.Rollins@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '067a3ccecb97eb9052095ffe7e8b8e5d4fa71d16df01a952ab9f4638b199a04ad970246482d38627eb86cfe4868a0cff1baf05aa40daef7c395cc2d1b138c702'
